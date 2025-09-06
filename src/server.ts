3cd602a1175d3db981c1bbb7a5f738c701a45c7a
divspark <divyansh2004mhj@gmail.com>
2025-09-07 00:12:54 +0530

    Updated cors origins

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://69.62.70.22:8000/"],
+    origin: ["http://localhost:5173", "http://69.62.70.22:8000/", "http://69.62.70.22:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
